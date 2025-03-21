248b8c3d42ce3fbc2057ddada4f7183d4290b8dc
Carole <c.delfino@live.fr>
2025-03-21 11:39:58 +0100

    TP3

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,23 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+    public static void main(String[] args) {
+        byte b = 1;
+        short s = 200;
+        int i = 200000;
+        long l = 1_000_000L;
+
+        float f = 0.1f;
+        double db = 1.234;
+
+        char c = 'a';
+        String str = "abcd";
+        boolean bo = true;
+
+        System.out.println("String : " + str);
+
+        String randomString = "Voici le résultat d’un calcul : /n 1+5=6";
+        System.out.println(randomString);
+    }
+}
