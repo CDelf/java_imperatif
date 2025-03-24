14c6fd47ee3390f1763d02d9e86d3d67724088c2
Carole <c.delfino@live.fr>
2025-03-24 14:32:27 +0100

    TP autonomie algorithmie correction

--- src/fr/algorithmie/RechercheMin.java
+++ src/fr/algorithmie/RechercheMin.java
@@ -3,7 +3,7 @@ package fr.algorithmie;
 public class RechercheMin {
 
     public static void main(String[] args) {
-        // Rechercher le plus grand élément d'un tableau d'entier
+        // Rechercher le plus petit élément d'un tableau d'entier
         int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
         int min = array[0];
 
